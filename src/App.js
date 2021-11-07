a3ea6ad2064c8e56dca02d8a42068373dcce2e31
maziyar parsi <mparsi1992@gmail.com>
2021-11-07 18:10:33 +0330

    add-navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './App.css'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import { Header } from './Components'
+import { Navbar } from './Components'
 
 const App = () => {
   return (
     <div className="app">
-      <div className="header">
-        <Header />
+      <div className="navbar">
+        <Navbar />
       </div>
       <div className="main"></div>
       <div className="footer"></div>
