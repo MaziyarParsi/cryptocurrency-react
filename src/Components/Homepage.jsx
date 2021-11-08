8e9b7f4c755c8a4dd32a9ddc7c44ca667d5f7952
maziyar parsi <mparsi1992@gmail.com>
2021-11-08 11:49:41 +0330

    add homepage info

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography,Row,Col,Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+
+
+
 
 const Homepage = () => {
+ const {Title}= Typography
+
+
  return (
-  <div>
-   this is homepag
-  </div>
+  <>
+  <Title level={2} className='heading'>Global Crypto stats</Title>
+  <Row>
+     <Col span={12} >
+       <Statistic title='Total Cryptocurrencies' value='5'/>
+     </Col>
+     <Col span={12}>
+       <Statistic title='Total Exchanges' value='5'/>
+     </Col>
+     <Col span={12}>
+       <Statistic title='Total Market Cap' value='5'/>
+     </Col>
+     <Col span={12}>
+       <Statistic title='Total Market Cap' value='5'/>
+     </Col>
+     <Col span={12}>
+       <Statistic title='Total Marketsp' value='5'/>
+     </Col>
+  </Row>
+  
+  </>
  )
 }
 
